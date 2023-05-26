e61e32eb7b46cbeeef9e091070c3560e67f6944d
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-25 21:48:49 -0300

    upd: refatorando para build

--- arvore.c
+++ arvore.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 
 enum coloracao { Vermelho, Preto };
 
@@ -18,6 +17,13 @@ typedef struct arvore {
     struct no* nulo;
 } Arvore;
 
+Arvore* cria() {
+    Arvore *arvore = malloc(sizeof(Arvore));
+    arvore->raiz = NULL;
+
+    return arvore;
+}
+
 No* criarNo(Arvore* arvore, No* pai, int valor) {
     No* no = malloc(sizeof(No));
 
@@ -28,33 +34,63 @@ No* criarNo(Arvore* arvore, No* pai, int valor) {
     no->cor = Vermelho;
 }
 
-No* adicionarNo(No* no, int valor) {
-    if (valor > no->valor) {
-        if (no->direita == NULL) {
-            No* novo = criarNo(valor);
-            novo->pai = no;
-            no->direita = novo;
+void rotacionarEsquerda(Arvore* arvore, No* no) {
+    No* direita = no->direita;
+    no->direita = direita->esquerda;
+    if (direita->esquerda != arvore->nulo)
+        direita->esquerda->pai = no; //Se houver filho à esquerda em direita, ele será pai do nó
+    direita->pai = no->pai; //Ajusta no pai do nó à direita
 
-            return novo;
-        } else {
-            return adicionarNo(no->direita, valor);
-        }
-    } else {
-        if (no->esquerda == NULL) {
-            No* novo = criarNo(valor);
-            novo->pai = no;
-            no->esquerda = novo;
-
-            return novo;
-        } else {
-            return adicionarNo(no->esquerda, valor);
-        }
-    }
+    if (no->pai == arvore->nulo)
+        arvore->raiz = direita; //Se nó for raiz, o nó direita será a nova raiz da árvore
+    else if (no == no->pai->esquerda)
+        no->pai->esquerda = direita; //Corrige relação pai-filho do novo pai (esquerda)
+    else
+        no->pai->direita = direita; //Corrige relação pai-filho do novo pai (direita)
+    direita->esquerda = no; //Corrige relação pai-filho entre o nó pivô e o nó à direita
+    no->pai = direita;
 }
 
-Arvore* cria() {
-    Arvore *arvore = malloc(sizeof(Arvore));
-    arvore->raiz = NULL;
+void rotacionarDireita(Arvore* arvore, No* no) {
+    No* esquerda = no->esquerda;
+    no->esquerda = esquerda->direita;
+    if (esquerda->direita != arvore->nulo)
+        esquerda->direita->pai = no; //Se houver filho à direita em esquerda, ele será pai do nó
+    esquerda->pai = no->pai; //Ajusta no pai do nó à esquerda
 
-    return arvore;
+    if (no->pai == arvore->nulo)
+        arvore->raiz = esquerda; //Se nó for raiz, o nó esquerda será a nova raiz da árvore
+    else if (no == no->pai->esquerda)
+        no->pai->esquerda = esquerda; //Corrige relação pai-filho do novo pai (esquerda)
+    else
+        no->pai->direita = esquerda; //Corrige relação pai-filho do novo pai (direita)
+    esquerda->direita = no; //Corrige relação pai-filho entre o nó pivô e o nó à esquerda
+    no->pai = esquerda;
+}
+
+void balancear(Arvore* arvore, No* no) {
+    while (no->pai->cor == Vermelho) { //Garante que todos os níveis foram balanceados
+        if (no->pai == no->pai->pai->esquerda) {
+            No *tio = no->pai->pai->direita;
+
+            if (tio->cor == Vermelho) {
+                tio->cor = Preto; //Resolve caso 2
+                no->pai->cor = Preto;
+                no->pai->pai->cor = Vermelho;
+                no = no->pai->pai; //Vai para o nível anterior
+            } else {
+                if (no == no->pai->direita) {
+                    no = no->pai; //Vai para o nível anterior
+                    rotacionarEsquerda(arvore, no); //Resolve caso 3
+                } else {
+                    no->pai->cor = Preto; //Resolve caso 4
+                    no->pai->pai->cor = Vermelho;
+                    rotacionarDireita(arvore, no->pai->pai);
+                }
+            }
+        } else {
+            //Repete o mesmo código do bloco if, invertendo o lado dos direita e esquerda
+        }
+    }
+    arvore->raiz->cor = Preto; //Resolve caso 1
 }
--- main.c
+++ main.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
 #include "arvore.c"
-#include "rotacionar.c"
 
 int main() {
     Arvore* arvore = cria();
-    adicionar(arvore, 4);
-    adicionar(arvore, 2);
-    adicionar(arvore, 8);
-    adicionar(arvore, 1);
-    adicionar(arvore, 3);
-    adicionar(arvore, 6);
-    adicionar(arvore, 9);
-    adicionar(arvore, 5);
-    adicionar(arvore, 7);
 
     return 0;
 }
